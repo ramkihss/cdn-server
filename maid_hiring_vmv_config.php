1ec3ca59fd993d0d78298458a127b606755ca356
ramkihss <156517719+ramkihss@users.noreply.github.com>
2026-03-23 21:08:19 +0530

    Update maid_hiring_vmv_config.php

--- maid_hiring_vmv_config.php
+++ maid_hiring_vmv_config.php
@@ -1,1 +1,13 @@
 
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "maid_service_hiring";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+?>
