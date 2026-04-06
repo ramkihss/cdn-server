f1ea1a8c3d5987f8ece66b694c4a448be48d5d33
ramkihss <156517719+ramkihss@users.noreply.github.com>
2026-04-06 09:55:01 +0530

    Create candle_db.php

---
+++ candle_db.php
@@ -0,0 +1,16 @@
+<?php
+// config.php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "dealport";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+?>
