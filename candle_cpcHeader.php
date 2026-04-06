0f58e734495cc9503171ee9de9dac1b7b7b8d943
ramkihss <156517719+ramkihss@users.noreply.github.com>
2026-04-06 09:53:59 +0530

    Create candle_cpcHeader.php

---
+++ candle_cpcHeader.php
@@ -0,0 +1,117 @@
+  <header id="header" class="site-header">
+      <nav id="header-nav" class="navbar navbar-expand-lg p-3 p-lg-0">
+        <div class="container">
+          <a class="navbar-brand d-lg-none" href="index.html">
+            <img src="images/main-logo.png" class="logo img-fluid">
+          </a>
+          <button class="navbar-toggler d-flex d-lg-none order-3 p-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#bdNavbar" aria-controls="bdNavbar" aria-expanded="false" aria-label="Toggle navigation">Menu</button>
+          <div class="offcanvas offcanvas-end" tabindex="-1" id="bdNavbar" aria-labelledby="bdNavbarOffcanvasLabel">
+            <div class="offcanvas-header px-4 pb-0">
+              <a class="navbar-brand" href="index.html">
+                <img src="images/main-logo.png" class="logo img-fluid">
+              </a>
+              <button type="button" class="btn-close btn-close-black" data-bs-dismiss="offcanvas" aria-label="Close" data-bs-target="#bdNavbar"></button>
+            </div>
+            <div class="offcanvas-body d-block">
+              <ul id="navbar" class="navbar-nav w-100 py-4 d-none d-lg-flex justify-content-between align-items-center border-bottom border-dark">
+                <a class="navbar-brand d-none d-lg-block me-0" href="index.html">
+                  <img src="images/main-logo.png" class="logo img-fluid">
+                </a>
+    
+                <ul class="list-unstyled d-lg-flex justify-content-between align-items-center">
+                  <li class="nav-item ms-5 search-item">
+                    <div id="search-bar" class="border-right d-lg-block">
+                      <form action="" autocomplete="on">
+                        <input id="search" class="text-dark" name="search" type="text" placeholder="Search Here...">
+                        <a type="submit" class="nav-link me-0" href="#">Search</a>
+                      </form>
+                    </div>
+                  </li>
+                  <li class="cart-dropdown nav-item ms-5 dropdown">
+                    <a class="nav-link dropdown-toggle me-0" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Cart(2)</a>
+                    <div class="dropdown-menu dropdown-menu-end p-3">
+                      <h4 class="d-flex justify-content-between align-items-center mb-3">
+                        <span class="text-primary">Your cart</span>
+                        <span class="badge bg-primary rounded-pill">2</span>
+                      </h4>
+                      <ul class="list-group mb-3">
+                        <li class="list-group-item bg-transparent border-dark d-flex justify-content-between lh-sm">
+                          <div>
+                            <h5 class="card-title fs-3 text-capitalize">
+                              <a href="single-product.html">White Cement</a>
+                            </h5>
+                            <small class="text-body-secondary">Made with cement cover.</small>
+                          </div>
+                          <span class="text-primary">money(33</span>
+                        </li>
+                        <li class="list-group-item bg-transparent border-dark d-flex justify-content-between lh-sm">
+                          <div>
+                            <h5 class="card-title fs-3 text-capitalize">
+                              <a href="single-product.html">Heavy Red</a>
+                            </h5>
+                            <small class="text-body-secondary">Pleasant light fragnance.</small>
+                          </div>
+                          <span class="text-primary">money(42</span>
+                        </li>
+                        <li class="list-group-item bg-transparent border-dark d-flex justify-content-between">
+                          <span class="text-uppercase"><b>Total (Rs.)</b></span>
+                          <strong>money(75</strong>
+                        </li>
+                      </ul>
+                      <div class="d-flex flex-wrap justify-content-center">
+                        <a href="cart.html" class="w-100 btn btn-dark mb-1" type="submit">View Cart</a>
+                        <a href="checkout.html" class="w-100 btn btn-primary" type="submit">Go to checkout</a>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="nav-item ms-5">
+                    <a class="nav-link me-0" href="login.html">Account</a>
+                  </li>
+                </ul>
+              </ul>
+              <ul class="list-unstyled d-lg-flex m-0 py-2">
+                <li class="nav-item me-5">
+                  <a class="nav-link active ms-0" href="index.html">Home</a>
+                </li>
+                <li class="nav-item me-5">
+                  <a class="nav-link ms-0" href="about.html">About</a>
+                </li>
+                <li class="nav-item me-5">
+                  <a class="nav-link ms-0" href="shop.html">Shop</a>
+                </li>
+        
+                <li class="nav-item me-5">
+                  <a class="nav-link ms-0" href="contact.html">Contact</a>
+                </li>
+                <li class="nav-item me-5 dropdown">
+                  <a class="nav-link dropdown-toggle ms-0" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Pages<svg class="bi" width="18" height="18"><use xlink:href="#chevron-down"></use></svg></a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a href="about.html" class="dropdown-item fs-5 fw-medium">About </a>
+                    </li>
+                    <li>
+                      <a href="shop.html" class="dropdown-item fs-5 fw-medium">Shop </a>
+                    </li>
+       
+                    <li>
+                      <a href="login.html" class="dropdown-item fs-5 fw-medium">Account </a>
+                    </li>
+                    <li>
+                      <a href="cart.html" class="dropdown-item fs-5 fw-medium">Cart </a>
+                    </li>
+                    <li>
+                      <a href="checkout.html" class="dropdown-item fs-5 fw-medium">Checkout </a>
+                    </li>
+         
+                    <li>
+                      <a href="contact.html" class="dropdown-item fs-5 fw-medium">Contact </a>
+                    </li>
+                  </ul>
+                </li>
+       
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
