ea39c34e51185ed0c770e9d17168adfd74fd1144
ramkihss <156517719+ramkihss@users.noreply.github.com>
2026-04-07 20:20:54 +0530

    Create grocery_footer_cpc.php

---
+++ grocery_footer_cpc.php
@@ -0,0 +1,37 @@
+<div class="footer">
+		<div class="container">
+			<div class="col-md-4 w3_footer_grid">
+				<h3>information</h3>
+				<ul class="w3_footer_grid_list">
+					<li><a href="about.php">About Us</a></li>
+					<li><a href="mail.php">Contact Us</a></li>
+				</ul>
+			</div>
+			<div class="col-md-4 w3_footer_grid">
+				<h3>what in stores</h3>
+				<ul class="w3_footer_grid_list">
+					<li><a href="products.php">Grocery</a></li>
+					<li><a href="products.php">Branded </a></li>
+					<li><a href="products.php">Households</a></li>
+				</ul>
+			</div>
+			<div class="col-md-4 w3_footer_grid">
+				<h3>Contact Us</h3>
+				<ul class="w3_footer_grid_list1">
+                    <ul class="agileits_social_icons">
+                        <li><a href="#" class="facebook"><i class="fa fa-facebook" aria-hidden="true"></i> </a></li>
+                        <li><a href="#" class="twitter"><i class="fa fa-twitter" aria-hidden="true"></i> </a></li>
+                        <li><a href="#" class="instagram"><i class="fa fa-instagram" aria-hidden="true"></i> </a></li>
+				    </ul>
+                    <ul class="phone_email">
+                        <li><i class="fa fa-phone" aria-hidden="true"></i> <a href="tel:+919979331605">(+91) 9979331605</a></li>
+                        <li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:store@grocery.com">store@grocery.com</a></li>
+				    </ul>
+				</ul>
+			</div>
+			<div class="clearfix"></div>
+			<div class="wthree_footer_copy">
+				<p>© <?=date('Y')?> Grocery Store. All rights reserved | Design by <a href="http://w3layouts.com/">Saloni Yadav and Yug Yadav</a></p>
+			</div>
+		</div>
+</div>
