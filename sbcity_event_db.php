4a66a4d987ea60967a40010977209830dd2cd37d
ramkihss <156517719+ramkihss@users.noreply.github.com>
2026-04-24 12:17:53 +0530

    Update sbcity_event_db.php

--- sbcity_event_db.php
+++ sbcity_event_db.php
@@ -1,28 +1,3 @@
-<?php
-class Connection {
-    
-    private static $connect = NULL;
-    
-    public static function getInstance() {
-        if (Connection::$connect === NULL) {
-            // connect to the database
-            $host = "localhost";
-			$database = "year2project";
-			$username = "root";
-			$password = "";
+<?php 
 
-            $dsn = "mysql:host=" . $host . ";dbname=" . $database;
-            Connection::$connect = new PDO($dsn, $username, $password);
-            if (!Connection::$connect) {
-                die("Could not connect to database");
-            }
-        }
-        
-        return Connection::$connect;
-    }
-    
-    public static function getMySQLDate($date) {
-        $date_arr  = explode('-', $date);
-        return $date_arr[2] . '-' . $date_arr[1] . '-' . $date_arr[0];
-    }
-}
+$conn= new mysqli('localhost','root','','event_db')or die("Could not connect to mysql".mysqli_error($con));
